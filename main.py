2c4ad513b6fc17edcea7a0a18240036987e16238
Divya Sawant <86359301+DivyaRsawant@users.noreply.github.com>
2023-04-06 13:34:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import cv2
 
 source = "tom.jpg"
-destination = "NewImage.jpeg"
+destination = "NewImage.jpeg"  
+#can change formate jpeg, jpg, png the file will be made according to the formate given
+
 scale_percent = 50
 
 src = cv2.imread(source, cv2.IMREAD_UNCHANGED)
@@ -10,4 +12,4 @@ new_width = int(src.shape[1] * scale_percent / 100)
 new_height = int(src.shape[0] * scale_percent / 100)
 output = cv2.resize(src, (new_width, new_height))
 cv2.imwrite(destination, output)
-# cv2.waitKey(0)
+# cv2.waitKey(0) 
