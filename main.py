0daea36bdb40af1044ccc06abf07a3e731b9ed8e
Divya Sawant <86359301+DivyaRsawant@users.noreply.github.com>
2023-04-06 13:31:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+
+source = "tom.jpg"
+destination = "NewImage.jpeg"
+scale_percent = 50
+
+src = cv2.imread(source, cv2.IMREAD_UNCHANGED)
+# cv2.imshow('title', src)
+new_width = int(src.shape[1] * scale_percent / 100)
+new_height = int(src.shape[0] * scale_percent / 100)
+output = cv2.resize(src, (new_width, new_height))
+cv2.imwrite(destination, output)
+# cv2.waitKey(0)
